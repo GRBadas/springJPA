9badc913cba9ce23d1fe8bff2b140fabb758d4cb
Gabriel Badaró <grbadas@gmail.com>
2023-02-02 22:38:14 -0300

    fisrt commit

---
+++ src/main/java/com/badas/badasfood/BadasfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.badas.badasfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadasfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadasfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badas/badasfood/BadasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badas.badasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
