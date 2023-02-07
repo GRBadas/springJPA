5dae67db673bcb5492116b1f41b315f4fea77ee7
Gabriel Badaró <grbadas@gmail.com>
2023-02-06 22:29:47 -0300

    update

--- src/main/java/com/badas/badasfood/listener/NotificacaoService.java
+++ src/main/java/com/badas/badasfood/listener/NotificacaoService.java
@@ -1,16 +1,24 @@
 package com.badas.badasfood.listener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import com.badas.badasfood.di.notificacao.NivelUrgencia;
+import com.badas.badasfood.di.notificacao.Notificador;
+import com.badas.badasfood.di.notificacao.TipoDoNotificador;
 import com.badas.badasfood.di.service.ClienteAtivadoEvent;
 
 @Component
 public class NotificacaoService {
+	
+	@TipoDoNotificador(NivelUrgencia.BAIXA_PRIORIDADE)
+	@Autowired
+	private Notificador notificador;
 
 	@EventListener
 	public void clienteAtivadoListener(ClienteAtivadoEvent event) {
-		System.out.println("Cliente" + event.getCliente().getNome() + "agora está ativo ! ");
+		notificador.notificar(event.getCliente(), "Seu cadastro no sistema está ativo !");
 	}
 	
 }
