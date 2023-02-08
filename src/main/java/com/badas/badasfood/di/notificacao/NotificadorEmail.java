1734c7942c2a6fb9768ee614353ba9c8719b6b1d
Gabriel Badaró <grbadas@gmail.com>
2023-02-08 14:54:25 -0300

    fix

--- src/main/java/com/badas/badasfood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/badas/badasfood/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,5 @@
 package com.badas.badasfood.di.notificacao;
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.badas.badasfood.di.modelo.Cliente;
 
@@ -8,19 +7,14 @@ import com.badas.badasfood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	@Value("${notificador.email.host-servidor}")
-	private String host;
-	
-	@Value("${notificador.email.porta-servidor}")
-	private Integer porta;
+	@Autowired
+	private NotificadorProperties properties;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.println("Host: " + host);
-		System.out.println("Porta: " + porta);
-		
-		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
-				cliente.getNome(), cliente.getEmail(), mensagem);
+		System.out.println("Host: " + properties.getHostServidor());
+		System.out.println("Porta: " + properties.getPortaServidor());
+		System.out.printf("Notificando %s através do email %ss: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
-
+	
 }
